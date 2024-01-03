d80581bf3d6565e6ba71f9cff13d866dbdd0251c
Chris Cunningham <39040164+lordcleverpork@users.noreply.github.com>
2024-01-03 10:06:48 -0800

    Update sensor.py
    Removed an incorrect comment

--- source/sensor.py
+++ source/sensor.py
@@ -1,5 +1,3 @@
-# This is a fake sensor that returns 100 when the letter 'a' is pressed and a random value between 0 and 100 when any other key is pressed.
-
 import random
 import keyboard
 import board
